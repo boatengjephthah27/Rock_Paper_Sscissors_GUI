3f6e21caab7437d9359e9e962fa07ba8bb1f9b50
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-05-03 06:17:26 +0000

    page2 gui done

--- RPSapp.py
+++ RPSapp.py
@@ -11,7 +11,7 @@ app.iconbitmap("./Assets/RPS_logo.ico")
 screen_width = app.winfo_screenwidth()
 screen_height = app.winfo_screenheight()
 window_height = 750
-window_width = 1000
+window_width = 883
 width_center = int(screen_width/2 - window_width/2)
 height_center = int(screen_height/2 - window_height/2)
 window_position = app.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
@@ -42,8 +42,8 @@ def nextpage():
     playpage.iconbitmap("./Assets/RPS_logo.ico")
     screen_width = app.winfo_screenwidth()
     screen_height = app.winfo_screenheight()
-    window_height = 800
-    window_width = 1200
+    window_height = 750
+    window_width = 883
     width_center = int(screen_width/2 - window_width/2)
     height_center = int(screen_height/2 - window_height/2)
     window_position = playpage.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
@@ -52,30 +52,54 @@ def nextpage():
     # make your choice label
     msglabel = tk.Label(playpage, text=".....MAKE A CHOICE.....", font=("Bungee Outline", "36", "bold"))
     msglabel.grid(row=0, column=1)
+    space = ttk.Label(playpage, text="  ")
+    space.grid(row=1)
+    space = ttk.Label(playpage, text="  ")
+    space.grid(row=2)
     plabel = tk.Label(playpage, text="Player: ", font=("Georgia", "20", "bold"))
     outlabel = tk.Label(playpage, text="Outcome: ", font=("Georgia", "20", "bold"))
     clabel = tk.Label(playpage, text="Computer: ", font=("Georgia", "20", "bold"))
-    plabel.grid(row=1, column=0)
-    outlabel.grid(row=1, column=1)
-    clabel.grid(row=1, column=2)
+    plabel.grid(row=3, column=0)
+    outlabel.grid(row=3, column=1)
+    clabel.grid(row=3, column=2)
 
+    space = ttk.Label(playpage, text="  ")
+    space.grid(row=4)
+    space = ttk.Label(playpage, text="  ")
+    space.grid(row=5)
     # creating the graphics outputs
-    pgraphics = ttk.Label(playpage, image=prock)
-    pgraphics.grid(row=2, column=0)
-    outgraphics = ttk.Label(playpage, image=win)
-    outgraphics.grid(row=2, column=1)
-    cgraphics = ttk.Label(playpage, image=comrock)
-    cgraphics.grid(row=2, column=2)
+    pgraphics = ttk.Label(playpage, image=pscissors)
+    pgraphics.grid(row=6, column=0, rowspan=1)
+    outgraphics = ttk.Label(playpage, image=draw)
+    outgraphics.grid(row=6, column=1, rowspan=1)
+    cgraphics = ttk.Label(playpage, image=comscissors)
+    cgraphics.grid(row=6, column=2, rowspan=1)
+    space = ttk.Label(playpage, text="  ")
+    space.grid(row=7)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=8)
 
     # creating the RPS buttons
     rbutton = tk.Button(playpage, text="Rock", font=("Arial Black", "26", "bold"))
-    rbutton.grid(row=6, column=0)
+    rbutton.grid(row=9, column=0)
     pbutton = tk.Button(playpage, text="Paper", font=("Arial Black", "26", "bold"))
-    pbutton.grid(row=6, column=2)
+    pbutton.grid(row=9, column=2)
     sbutton = tk.Button(playpage, text="Scissors", font=("Arial Black", "26", "bold"))
-    sbutton.grid(row=7, column=1)
-
-
+    sbutton.grid(row=10, column=1)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=11)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=12)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=13)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=14)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=14)
+    spac = ttk.Label(playpage, text="  ")
+    spac.grid(row=15)
+    signature = ttk.Label(playpage, text="© App by: Boateng Jephthah Agyenim", font=("Signatura","7"))
+    signature.grid(row=16, column=1)
 
 
     
