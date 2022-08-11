aae0e397aeae3565c8fa2b5deeba58a2fd9c727a
Boateng Jephthah Agyenim <Boatengjephthah@gmail.com>
2022-08-11 10:39:57 +0000

    functions done for btns

--- RPSapp.py
+++ RPSapp.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import random as ran
 from tkinter import messagebox
+import time as t
 
 
 
@@ -97,6 +98,133 @@ def page1():
 
 
 
+
+def win():
+    global output
+    output = Toplevel(app)
+    output.config(
+        padx=50,
+        pady=50
+    )
+    output.title("Rock Paper Scissors")
+    output.iconbitmap("./Assets/RPS_logo.ico")
+    output.config(padx=50, pady=60)
+
+    screen_width = output.winfo_screenwidth()
+    screen_height = output.winfo_screenheight()
+    window_height = 450
+    window_width = 400
+    width_center = int(screen_width/2 - window_width/2)
+    height_center = int(screen_height/2 - window_height/2)
+    window_position = output.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
+    output.resizable(False, False)
+    
+    label = Label(
+        output,
+        image=p_win,
+        pady=10
+    )
+    label.grid(row=0, column=0)
+    
+    again = Button(
+        output,
+        text="AGAIN",
+        font=("courier", 24, "bold"),
+        padx=10,
+        pady=10,
+        command=dest
+    )
+    again.grid(row=1, column=0)
+    
+    
+    
+    
+def dest():
+    output.destroy()
+    
+    
+    
+
+def lose():
+    global output
+    output = Toplevel(app)
+    output.config(
+        padx=50,
+        pady=50
+    )
+    output.title("Rock Paper Scissors")
+    output.iconbitmap("./Assets/RPS_logo.ico")
+    output.config(padx=50, pady=60)
+
+    screen_width = output.winfo_screenwidth()
+    screen_height = output.winfo_screenheight()
+    window_height = 450
+    window_width = 430
+    width_center = int(screen_width/2 - window_width/2)
+    height_center = int(screen_height/2 - window_height/2)
+    window_position = output.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
+    output.resizable(False, False)
+    
+    label = Label(
+        output,
+        image=p_lose,
+        pady=10
+    )
+    label.grid(row=0, column=0)
+    
+    again = Button(
+        output,
+        text="AGAIN",
+        font=("courier", 24, "bold"),
+        padx=10,
+        pady=10,
+        command=dest
+    )
+    again.grid(row=1, column=0)
+    
+
+
+
+def draw():
+    global output
+    output = Toplevel(app)
+    output.config(
+        padx=50,
+        pady=50
+    )
+    output.title("Rock Paper Scissors")
+    output.iconbitmap("./Assets/RPS_logo.ico")
+    output.config(padx=50, pady=60)
+
+    screen_width = output.winfo_screenwidth()
+    screen_height = output.winfo_screenheight()
+    window_height = 460
+    window_width = 500
+    width_center = int(screen_width/2 - window_width/2)
+    height_center = int(screen_height/2 - window_height/2)
+    window_position = output.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
+    output.resizable(False, False)
+    
+    label = Label(
+        output,
+        image=p_draw,
+        pady=10
+    )
+    label.grid(row=0, column=0)
+    
+    again = Button(
+        output,
+        text="AGAIN",
+        font=("courier", 24, "bold"),
+        padx=10,
+        pady=10,
+        command=dest
+    )
+    again.grid(row=1, column=0)
+    
+    
+
+
 def record():
     
     if len(name.get()) < 1:
@@ -108,17 +236,77 @@ def record():
         fpage.destroy()
 
 
+
+
 def choiceRock():
-    pass
+    global p_choice
+    p_choice = "Rock"
+    canvas1.itemconfig(p_image, image=prock)
+        
+    check()
+
+    if c_choice == "c_rock":
+        print("rock")
+        draw()
+    elif c_choice == "c_paper":
+        print("paper")
+        lose()
+    elif c_choice == "c_scissors":
+        print("scissors")
+        win()
+    
+
 
 
 def choicePaper():
-    pass
+    global p_choice
+    p_choice = "Paper"
+    canvas1.itemconfig(p_image, image=ppaper)
+
+    check()    
+    
+    if c_choice == "c_rock":
+        print("rock")
+        win()
+    elif c_choice == "c_paper":
+        print("paper")
+        draw()
+    elif c_choice == "c_scissors":
+        print("scissors")
+        lose()
+
+
 
 
 def choiceScissors():
-    pass
+    global p_choice
+    p_choice = "Scissors"
+    canvas1.itemconfig(p_image, image=pscissors)
 
+    check()
+    
+    if c_choice == "c_rock":
+        print("rock")
+        lose()
+    elif c_choice == "c_paper":
+        print("paper")
+        win()
+    elif c_choice == "c_scissors":
+        print("scissors")
+        draw()
+
+
+def check():
+    
+    global c_choice
+    c_choice = ran.choice(["c_rock", "c_paper", "c_scissors"])
+    
+    if c_choice == "c_rock":
+        canvas2.itemconfig(com_image, image=comrock)
+    elif c_choice == "c_paper":
+        canvas2.itemconfig(com_image, image=compaper)
+    else:
+        canvas2.itemconfig(com_image, image=comscissors)
 
 
 
@@ -126,6 +314,7 @@ def choiceScissors():
 
 
 # ********************************************************** GUI ***********************************************************
+
 
 
 
@@ -147,9 +336,9 @@ window_position = app.geometry(f"{window_width}x{window_height}+{width_center}+{
 app.resizable(False, False)
 
 # adding images
-win = PhotoImage(file="./Assets/won.png")
-lost = PhotoImage(file="./Assets/lost.png")
-draw = PhotoImage(file="./Assets/draw.png")
+p_win = PhotoImage(file="./Assets/won.png")
+p_lose = PhotoImage(file="./Assets/lost.png")
+p_draw = PhotoImage(file="./Assets/draw.png")
 prock = PhotoImage(file="./Assets/player_rock.png")
 ppaper = PhotoImage(file="./Assets/player_paper.png")
 pscissors = PhotoImage(file="./Assets/player_scissors.png")
@@ -181,9 +370,9 @@ canvas1 = Canvas(
     height=300,
     highlightthickness= 0
 )
-canvas1.create_image(
+p_image = canvas1.create_image(
     110,150,
-    image = ppaper,
+    image = "",
 )
 canvas1.grid(row=1, column=0)
 
@@ -201,9 +390,9 @@ canvas2 = Canvas(
     height=220,
     highlightthickness= 0
 )
-canvas2.create_image(
+com_image = canvas2.create_image(
     110,110,
-    image = compaper
+    image = ""
 )
 canvas2.grid(row=1, column=2)
 
@@ -213,7 +402,8 @@ rbutton = Button(
     text="Rock", 
     font=("Arial Black", "26", "bold"),
     padx=20,
-    pady=20
+    pady=20,
+    command=choiceRock
 )
 rbutton.grid(row=2, column=0)
 
@@ -221,7 +411,8 @@ pbutton = Button(
     text="Paper",
     font=("Arial Black", "26", "bold"),
     padx=20,
-    pady=20
+    pady=20,
+    command=choicePaper
 )
 pbutton.grid(row=3, column=1)
 
@@ -229,7 +420,8 @@ sbutton = Button(
     text="Scissors", 
     font=("Arial Black", "26", "bold"),
     padx=20,
-    pady=20
+    pady=20,
+    command=choiceScissors
 )
 sbutton.grid(row=2, column=2)
 
@@ -245,176 +437,8 @@ signature.grid(row=4, column=1)
 
 
 
-# # Defining front page
-# photo = tk.PhotoImage(file="./Assets/RPS_logo.png")
-# label = ttk.Label(app, image=photo)
-# # label.grid(row=0, column=0)
-# label.pack()
-
-
-
-
-#building the second page from continue button
-# def nextpage():
-#     c_names = ["Siri", "Bixbi", "Cortana", "Jarvis", "Friday", "Jexi", "Data", "Bishop", "C3PO", "R2D2", "Ava", "T-800", "Alexa"]   
-#     comp_name = ran.choice(c_names)
-#     playpage = tk.Toplevel()
-#     playpage.iconbitmap("./Assets/RPS_logo.ico")
-#     screen_width = app.winfo_screenwidth()
-#     screen_height = app.winfo_screenheight()
-#     window_height = 750
-#     window_width = 883
-#     width_center = int(screen_width/2 - window_width/2)
-#     height_center = int(screen_height/2 - window_height/2)
-#     window_position = playpage.geometry(f"{window_width}x{window_height}+{width_center}+{height_center}")
-#     # playpage.resizable(False, False)
-
-#     # make your choice label
-#     msglabel = tk.Label(playpage, text=".....MAKE A CHOICE.....", font=("Bungee Outline", "36", "bold"))
-#     msglabel.grid(row=0, column=1)
-#     space = ttk.Label(playpage, text="  ")
-#     space.grid(row=1)
-#     space = ttk.Label(playpage, text="  ")
-#     space.grid(row=2)
-#     plabel = tk.Label(playpage, text=f"Player: {p_name}", font=("Georgia", "20", "bold"))
-#     outlabel = tk.Label(playpage, text="Outcome: ", font=("Georgia", "20", "bold"))
-#     clabel = tk.Label(playpage, text=f"Computer: {comp_name}", font=("Georgia", "20", "bold"))
-#     plabel.grid(row=3, column=0)
-#     outlabel.grid(row=3, column=1)
-#     clabel.grid(row=3, column=2)
-
-#     space = ttk.Label(playpage, text="  ")
-#     space.grid(row=4)
-#     space = ttk.Label(playpage, text="  ")
-#     space.grid(row=5)
-#     # creating the graphics outputs
-#     pgraphics = ttk.Label(playpage, image=pscissors)
-#     pgraphics.grid(row=6, column=0, rowspan=1)
-#     outgraphics = ttk.Label(playpage, image=draw)
-#     outgraphics.grid(row=6, column=1, rowspan=1)
-#     cgraphics = ttk.Label(playpage, image=comscissors)
-#     cgraphics.grid(row=6, column=2, rowspan=1)
-#     space = ttk.Label(playpage, text="  ")
-#     space.grid(row=7)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=8)
-
-#     # creating the RPS buttons
-#     rbutton = tk.Button(playpage, text="Rock", font=("Arial Black", "26", "bold"))
-#     rbutton.grid(row=9, column=0)
-
-#     pbutton = tk.Button(playpage, text="Paper", font=("Arial Black", "26", "bold"))
-#     pbutton.grid(row=9, column=2)
-
-#     sbutton = tk.Button(playpage, text="Scissors", font=("Arial Black", "26", "bold"))
-#     sbutton.grid(row=10, column=1)
-
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=11)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=12)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=13)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=14)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=14)
-#     spac = ttk.Label(playpage, text="  ")
-#     spac.grid(row=15)
-#     signature = ttk.Label(playpage, text="© App by: Boateng Jephthah Agyenim", font=("Signatura","7"))
-#     signature.grid(row=16, column=1)
-
-#     # binding keys to the buttons
-#     rbutton.bind("<R>", choiceRock)
-#     rbutton.bind("<r>", choiceRock)
-#     pbutton.bind("<P>", choicePaper)
-#     pbutton.bind("<p>", choicePaper)
-#     sbutton.bind("<S>", choiceScissors)
-#     sbutton.bind("<s>", choiceScissors)
-
-
-    
-
-
-# creating the name entry key binding function for <Return key>
-# def npage(event):
-#     nextpage()
-    
-# creating the name and entry widget 
-# nameFrame = ttk.Frame(app)
-# name = ttk.Label(nameFrame, text="Player Name: ", font=("Times New Roman", "18", "bold"))
-# name_entry = tk.Entry(nameFrame, font=("Times New Roman", "20"), borderwidth=3, width=40, bg="black", fg="white")
-# name.grid(row=0, column=0)
-# name_entry.grid(row=2, column=0)
-# nameFrame.pack()
-# name_entry.bind('<Key-Return>', npage)
-# p_name = name_entry.get()
-
-# # creating the continue button
-# continueButton = ttk.Button(text="Continue", command=nextpage)
-# continueButton.pack()
-
-# # creating my signature
-# sign = ttk.Label(app, text="© App by: Boateng Jephthah Agyenim", font=("Signatura","7"))
-# sign.pack()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 page1()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
